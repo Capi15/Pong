ef975d880187cae25a4faf922403a70f3d1c875a
Capi15 <capicapi1515@gmail.com>
2021-05-05 11:43:06 +0100

    Updated

--- app/js/menuscenemobile.js
+++ app/js/menuscenemobile.js
@@ -2,24 +2,28 @@ class MenuSceneMobile extends Phaser.Scene {
     constructor() {
         super({ key: 'MenuSceneMobile' });
     }
+    preload() {
+            this.load.html("form", "form.html");
+    }
 
     create() {
-        this.buttonJogar = this.add
-            .image(
-                this.game.canvas.width / 2,
-                this.game.canvas.height / 2,
-                'startButtonImg'
-            )
-            .setScale(0.3)
-            .setInteractive({ useHandCursor: true });
+        this.nameInput = this.add.dom(this.sys.game.canvas.width / 2, this.sys.game.canvas.height / 2).createFromCache("form");
 
-        //click no butão jogar
-        this.buttonJogar.once(
-            'pointerdown',
-            function (pointer) {
-                this.scene.start('MobileScene');
-            },
-            this
-        );
+    this.message = this.add.text(this.sys.game.canvas.width / 2, this.sys.game.canvas.height / 2, "Digite o seu Nome", {
+        color: "#FFFFFF",
+        fontSize: 40,
+        fontStyle: "bold"
+    }).setOrigin(0.5);
+
+    this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
+    this.returnKey.on("down", event => {
+        let name = this.nameInput.getChildByName("name");
+        if(name.value != "") {
+            this.message.setText(name.value);
+            name.value = "";
+        }
+    });
+        //document.write("<input type=\"text\" name=\"username\" id=\"utext\">");
     }
 }
