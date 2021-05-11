9f3dce40fbab30bcadda4171ce88373868a17aee
Capi15 <capicapi1515@gmail.com>
2021-05-12 00:24:55 +0100

    Trying to add gameButtons

--- app/js/mobilescene.js
+++ app/js/mobilescene.js
@@ -5,9 +5,50 @@ class MobileScene extends Phaser.Scene {
 
     create() {
         const backgroundImage = this.add.image(
-            this.cameras.main.width / this.width,
-            this.cameras.main.height / this.height,
+            this.cameras.main.width / 2,
+            this.cameras.main.height / 2,
             'mBackImg'
         );
+
+        let scaleX = this.cameras.main.width / backgroundImage.width;
+        let scaleY = this.cameras.main.height / backgroundImage.height;
+
+        backgroundImage.setScale(scaleY).setScrollFactor(0);
+
+        const aQuarter = this.cameras.main.width / 4
+        let rightClick = this.add.sprite(
+            this.cameras.main.width/2  + aQuarter,
+            this.cameras.main.height / 2,
+            'arrowImg',
+            this.rightInput
+        ).setInteractive();
+
+
+        this.rightClick.on('pointerdown',
+        function (pointer) {
+            this.setScale(1.2);
+        },
+            this)
+
+        let leftClick = this.add.sprite(
+            this.cameras.main.width/2 - aQuarter,
+            this.cameras.main.height / 2,
+            'arrowImg',
+            this.leftInput
+        ).setInteractive();
+
+        this.leftClick.on('pointerdown',
+        function (pointer) {
+            this.setScale(1.2);
+        },
+            this)
+        
+        leftClick.setFlipX(-1)
+
+        rightClick.onInputOut.add(this.outRight, this);
+        leftClick.onInputOut.add(this.outLeft, this);
+
+
+
     }
 }
