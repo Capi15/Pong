ef5f1e1186ac310a4bc1b7dd8264e4873e9eb227
Capi15 <capicapi1515@gmail.com>
2021-07-19 16:18:00 +0100

    recheck

--- app/js/menuscenedesktop.js
+++ app/js/menuscenedesktop.js
@@ -1,143 +1,152 @@
 class MenuSceneDesktop extends Phaser.Scene {
-  noPlayers;
-  totalPlayers;
-  playerCount;
-  gamingList = [];
-  gamingListFunction;
-  waitingList = [];
+    noPlayers;
+    totalPlayers;
+    playerCount;
+    gamingList = [];
+    gamingListFunction;
+    waitingList = [];
 
-  timedEvent;
+    timedEvent;
 
-  timer;
-  total = 0;
-  noConn = 1;
+    timer;
+    total = 0;
+    noConn = 1;
 
-  constructor() {
-    super({ key: "MenuSceneDesktop" });
-  }
-  // ------------------------------  Create  ------------------------------
-  create() {
-    document.getElementById("game").classList.add("gameWindow");
+    constructor() {
+        super({ key: 'MenuSceneDesktop' });
+    }
+    // ------------------------------  Create  ------------------------------
+    create() {
+        document.getElementById('game').classList.add('gameWindow');
 
-    // ------------------------------  Numero de jogadores atuais  ------------------------------
-    this.noPlayers = 0;
-    this.totalPlayers = this.add.text(
-      10,
-      10,
-      "A aguardar jogadores...  " + this.noPlayers + "/6"
-    );
-
-    // ------------------------------  Texto inicio da partida / Timmer  ------------------------------
-    this.initialTime = 90;
-    this.timmerText = this.add.text(
-      500,
-      100,
-      "O jogo iniciará em " + this.formatTime(this.initialTime) + " segundos"
-    );
-    this.timedEvent = this.time.addEvent({
-      delay: 1000,
-      callback: this.onEvent,
-      callbackScope: this,
-      loop: true,
-    });
-
-    // ------------------------------  QR code Dispositivo Movel  ------------------------------
-    this.stringAndroid = this.add.text(130, 150, "Acesso android");
-    this.add.sprite(200, 300, "QrAndroid");
-
-    // ------------------------------  Texto Titulo Lista Jogadores  ------------------------------
-    this.stringLista = this.add.text(450, 200, "***  Lista de Jogadores  ***");
-    // ------------------------------  Texto com todos jogadores   ------------------------------
-    this.stringListaNomes = this.add.text(450, 250, " Nenhum ");
-
-    // ------------------------------ Var com info do Ecra principal + Emit para servidor  ------------------------------
-    var info = {
-      nome: "EcraPrincipal",
-      isDesktop: true,
-    };
-    socket.emit("novoPlayer", info);
-
-    // ------------------------------  Receção info players atuais / Numero Total e Nomes  ------------------------------
-    socket.on("JogadorEntrou", (listaJogadores) => {
-      console.log("L62");
-      this.noPlayers = listaJogadores.length;
-      this.totalPlayers = this.add.text(
-        "A aguardar jogadores...  " + this.noPlayers + "/6"
-      );
-      var y = 250;
-      let id = 0;
-      //this.stringListaNomes = this.setText(" A atualizar...");
-      for (let i = 0; i < listaJogadores.length; i++) {
-        console.log("L71 -> Ciclo " + i);
-        this.stringListaNomes = this.add.text(
-          450,
-          y,
-          i + 1 + "  ->  " + listaJogadores[i].nome
+        // ------------------------------  Numero de jogadores atuais  ------------------------------
+        this.noPlayers = 0;
+        this.totalPlayers = this.add.text(
+            10,
+            10,
+            'A aguardar jogadores...  ' + this.noPlayers + '/6'
         );
-        y += 20;
-        id = i;
-      }
-      //this.stringListaNomes.setText(        id + 1 + "  ->  " + listaJogadores[id].nome      );
-    });
-    //
 
-    socket.on("JogadorSaiu", (listaJogadores) => {
-      console.log("L62");
-      this.noPlayers = listaJogadores.length;
-      this.totalPlayers = this.add.text(
-        "A aguardar jogadores...  " + this.noPlayers + "/6"
-      );
-      var y = 250;
-      let id = 0;
-      //this.stringListaNomes = this.setText(" A atualizar...");
-      for (let i = 0; i < listaJogadores.length; i++) {
-        console.log("L71 -> Ciclo " + i);
-        this.stringListaNomes = this.add.text(
-          450,
-          y,
-          i + 1 + "  ->  " + listaJogadores[i].nome
+        // ------------------------------  Texto inicio da partida / Timmer  ------------------------------
+        this.initialTime = 90;
+        this.timmerText = this.add.text(
+            500,
+            100,
+            'O jogo iniciará em ' +
+                this.formatTime(this.initialTime) +
+                ' segundos'
         );
-        y += 20;
-        id = i;
-      }
-      //this.stringListaNomes.setText(        id + 1 + "  ->  " + listaJogadores[id].nome      );
-    });
-    //
+        this.timedEvent = this.time.addEvent({
+            delay: 1000,
+            callback: this.onEvent,
+            callbackScope: this,
+            loop: true,
+        });
 
-    console.log("L87");
-    socket.on("playerCount", (playerCount) => {
-      this.totalPlayers = this.setText("Na Fila -> " + playerCount + "/6");
-      console.log("Current player count: " + playerCount);
-    });
-  }
-  // ------------------------------  Update  ------------------------------
-  update() {
-    if (this.initialTime <= 0 && this.noPlayers >= 3) {
-      this.scene.start("DesktopScene");
-    } else if (this.noPlayers >= 7) {
-      this.scene.start("DesktopScene");
+        // ------------------------------  QR code Dispositivo Movel  ------------------------------
+        this.stringAndroid = this.add.text(130, 150, 'Acesso android');
+        this.add.sprite(200, 300, 'QrAndroid');
+
+        // ------------------------------  Texto Titulo Lista Jogadores  ------------------------------
+        this.stringLista = this.add.text(
+            450,
+            200,
+            '***  Lista de Jogadores  ***'
+        );
+        // ------------------------------  Texto com todos jogadores   ------------------------------
+        this.stringListaNomes = this.add.text(450, 250, ' Nenhum ');
+
+        // ------------------------------ Var com info do Ecra principal + Emit para servidor  ------------------------------
+        var info = {
+            nome: 'EcraPrincipal',
+            isDesktop: true,
+        };
+        socket.emit('novoPlayer', info);
+
+        // ------------------------------  Receção info players atuais / Numero Total e Nomes  ------------------------------
+        socket.on('JogadorEntrou', (listaJogadores) => {
+            console.log('L68');
+            this.noPlayers = listaJogadores.length;
+            this.totalPlayers = this.setText(
+                'A aguardar jogadores...  ' + this.noPlayers + '/6'
+            );
+            var y = 250;
+            let id = 0;
+            this.stringListaNomes = this.setText(' A atualizar...');
+            for (let i = 0; i < listaJogadores.length; i++) {
+                console.log('L71 -> Ciclo ' + i);
+                this.stringListaNomes = this.add.text(
+                    450,
+                    y,
+                    i + 1 + '  ->  ' + listaJogadores[i].nome
+                );
+                y += 20;
+                id = i;
+            }
+            //this.stringListaNomes.setText(        id + 1 + "  ->  " + listaJogadores[id].nome      );
+        });
+        //
+
+        socket.on('JogadorSaiu', (listaJogadores) => {
+            this.noPlayers = listaJogadores.length;
+            this.totalPlayers = this.setText(
+                'A aguardar jogadores...  ' + this.noPlayers + '/6'
+            );
+            var y = 250;
+            let id = 0;
+            this.stringListaNomes = this.setText(' A atualizar...');
+            for (let i = 0; i < listaJogadores.length; i++) {
+                console.log('L71 -> Ciclo ' + i);
+                this.stringListaNomes = this.add.text(
+                    450,
+                    y,
+                    i + 1 + '  ->  ' + listaJogadores[i].nome
+                );
+                y += 20;
+                id = i;
+            }
+            //this.stringListaNomes.setText(        id + 1 + "  ->  " + listaJogadores[id].nome      );
+        });
+        //
+
+        console.log('L87');
+        socket.on('playerCount', (playerCount) => {
+            this.totalPlayers = this.setText(
+                'Na Fila -> ' + playerCount + '/6'
+            );
+            console.log('Current player count: ' + playerCount);
+        });
+    }
+    // ------------------------------  Update  ------------------------------
+    update() {
+        if (this.initialTime <= 0 && this.noPlayers >= 3) {
+            this.scene.start('DesktopScene');
+        } else if (this.noPlayers >= 7) {
+            this.scene.start('DesktopScene');
+        }
+
+        while (this.initialTime < 0) {
+            this.initialTime = 90;
+            // this.timedEvent.pause = true;
+        }
+        this.totalPlayers.setText(
+            'A aguardar jogadores...  ' + this.noPlayers + '/6'
+        );
     }
 
-    while (this.initialTime < 0) {
-      this.initialTime = 90;
-      // this.timedEvent.pause = true;
+    formatTime(seconds) {
+        let minutes = Math.floor(seconds / 60);
+        let partInSeconds = seconds % 60;
+        partInSeconds = partInSeconds.toString().padStart(2, '0');
+        return `${minutes}:${partInSeconds}`;
     }
-    this.totalPlayers.setText(
-      "A aguardar jogadores...  " + this.noPlayers + "/6"
-    );
-  }
 
-  formatTime(seconds) {
-    let minutes = Math.floor(seconds / 60);
-    let partInSeconds = seconds % 60;
-    partInSeconds = partInSeconds.toString().padStart(2, "0");
-    return `${minutes}:${partInSeconds}`;
-  }
-
-  onEvent() {
-    this.initialTime -= 1; // One second
-    this.timmerText.setText(
-      "O jogo iniciará em " + this.formatTime(this.initialTime) + " segundos"
-    );
-  }
+    onEvent() {
+        this.initialTime -= 1; // One second
+        this.timmerText.setText(
+            'O jogo iniciará em ' +
+                this.formatTime(this.initialTime) +
+                ' segundos'
+        );
+    }
 }
--- app/server/server.js
+++ app/server/server.js
@@ -1,14 +1,14 @@
 //conneção ao servidor;
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketIO = require("socket.io");
-const { Socket } = require("dgram");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const { Socket } = require('dgram');
 const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
-const publicPath = path.join(__dirname, "/../");
+const publicPath = path.join(__dirname, '/../');
 
 let idJogador = 0; //============== id do jogador
 let limiteJogadores = 6; //======== máximo de jogadores
@@ -19,114 +19,114 @@ let isEcraPrincipal = false; //==== verifica se está a ser acedido no browser
 let SocketList = []; //============ Lista dos sockets (jogadores[moboile] + browser)
 
 dataEcra = {
-  nome: null,
-  isDesktop: null,
-  socket: null,
+    nome: null,
+    isDesktop: null,
+    socket: null,
 };
 
 dataJogadores = {
-  listaJogadores: [],
+    listaJogadores: [],
 };
 
 app.use(express.static(publicPath));
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}.`);
+    console.log(`Server is up on port ${port}.`);
 });
 
-io.on("connection", function (socket) {
-  SocketList.push(socket);
-  socket.on("novoPlayer", function (info) {
-    if (!isEcraPrincipal) {
-      if (info.isDesktop) {
-        dataEcra.nome = info.nome;
-        dataEcra.isDesktop = true;
-        dataEcra.socket = socket.id;
-        isEcraPrincipal = true;
-        ecraPrincipal = socket;
-        console.log("Web 46");
-      }
-    } else if (!info.isDesktop) {
-      playerCount++;
-      if (playerCount <= limiteJogadores) {
-        idJogador++;
-        //========================================== Adiciona Dispositivo á Lista
-        dataJogadores.listaJogadores.push({
-          id: idJogador,
-          nome: info.nome,
-          isDesktop: info.isDesktop,
-          play: info.play,
-          socket: socket.id,
-        });
-        //========================================== Adiciona Dispositivo á Lista
-        SocketList.push({
-          objSocket: socket,
-          id: idJogador,
-        });
-        ecraPrincipal.emit("playerCount", playerCount); //============================================== Envia contagem de jogadores para o Ecrã Principal
-        ecraPrincipal.emit(
-          "JogadorEntrou",
-          dataJogadores.listaJogadores //=========================== Envia Lista total de Jogadores para apresentação no Ecrã Principal
-        );
-        console.log(
-          "Mobile 65 => idJogador " + SocketList[SocketList.length - 1].id
-        );
-      } else {
-        console.log("Numero de jogadores chegou ao limite");
-        return;
-      }
-    } else if (info.isDesktop) {
-      socket.emit("valida", isEcraPrincipal);
-    }
-  });
-
-  socket.on("disconnect", function () {
-    dataJogadores.listaJogadores.forEach((element) => {
-      console.log("L84 -> " + element.id);
-      if (element.socket === socket.id) {
-        removePlayer(element, dataJogadores.listaJogadores);
-        console.log("removeu jogador");
-        ecraPrincipal.emit("JogadorSaiu", dataJogadores.listaJogadores);
-        console.log("L89 - JogadorSaiu");
-        limiteJogadores--;
-      }
+io.on('connection', function (socket) {
+    SocketList.push(socket);
+    socket.on('novoPlayer', function (info) {
+        if (!isEcraPrincipal) {
+            if (info.isDesktop) {
+                dataEcra.nome = info.nome;
+                dataEcra.isDesktop = true;
+                dataEcra.socket = socket.id;
+                isEcraPrincipal = true;
+                ecraPrincipal = socket;
+                console.log('Web 46');
+            }
+        } else if (!info.isDesktop) {
+            playerCount++;
+            if (playerCount <= limiteJogadores) {
+                idJogador++;
+                //========================================== Adiciona Dispositivo á Lista
+                dataJogadores.listaJogadores.push({
+                    id: idJogador,
+                    nome: info.nome,
+                    isDesktop: info.isDesktop,
+                    play: info.play,
+                    socket: socket.id,
+                });
+                //========================================== Adiciona Dispositivo á Lista
+                SocketList.push({
+                    objSocket: socket,
+                    id: idJogador,
+                });
+                ecraPrincipal.emit('playerCount', playerCount); //============================================== Envia contagem de jogadores para o Ecrã Principal
+                ecraPrincipal.emit(
+                    'JogadorEntrou',
+                    dataJogadores.listaJogadores //=========================== Envia Lista total de Jogadores para apresentação no Ecrã Principal
+                );
+                console.log(
+                    'Mobile 65 => idJogador ' +
+                        SocketList[SocketList.length - 1].id
+                );
+            } else {
+                console.log('Numero de jogadores chegou ao limite');
+                return;
+            }
+        } else if (info.isDesktop) {
+            socket.emit('valida', isEcraPrincipal);
+        }
     });
 
-    // SocketList.forEach((element) => {
-    //     console.log('L98 -> ' + element);
-    //     if (element.objSocket.id === socket.id) {
-    //         removePlayer2(element, SocketList);
-    //     }
-    // });
-    consoleLogListas();
-  });
+    socket.on('disconnect', function () {
+        dataJogadores.listaJogadores.forEach((element) => {
+            console.log('L84 -> ' + element.id);
+            if (element.socket === socket.id) {
+                removePlayer(element, dataJogadores.listaJogadores);
+                console.log('removeu jogador');
+                ecraPrincipal.emit('JogadorSaiu', dataJogadores.listaJogadores);
+                console.log('L89 - JogadorSaiu');
+                limiteJogadores--;
+            }
+        });
+
+        // SocketList.forEach((element) => {
+        //     console.log('L98 -> ' + element);
+        //     if (element.objSocket.id === socket.id) {
+        //         removePlayer2(element, SocketList);
+        //     }
+        // });
+        consoleLogListas();
+    });
 });
 
 //Elimina o Jogador da Lista pretendida com informação das sockets
 function removePlayer(obj, lista) {
-  let index = lista.indexOf(obj);
-  if (index > -1) {
-    lista.splice(index, 1);
-  }
+    let index = lista.indexOf(obj);
+    if (index > -1) {
+        lista.splice(index, 1);
+    }
 }
 
 function removePlayer2(obj, lista) {
-  let index = lista.indexOf(obj);
-  if (index > -1) {
-    lista.splice(index, 1);
-  }
+    let index = lista.indexOf(obj);
+    if (index > -1) {
+        lista.splice(index, 1);
+    }
 }
 
 //Valida Lisas com Socket depois de eliminar
 function consoleLogListas() {
-  console.log("Lista jogadores => ");
-  if (dataJogadores.listaJogadores > 0) {
-    dataJogadores.listaJogadores.forEach((element) => {
-      console.log(element.toString() + " ,");
-    });
-  }
+    if (dataJogadores.listaJogadores > 0) {
+        dataJogadores.listaJogadores.forEach((element) => {
+            console.log('Lista jogadores => ' + element.toString() + ' ,');
+        });
+    }
 
-  console.log("Lista do servidor => ");
-  SocketList.forEach((element) => {
-    console.log(element.idJogador + ",");
-  });
+    console.log('Lista do servidor => ');
+    SocketList.forEach((element) => {
+        console.log(element.idJogador + ',');
+    });
 }
