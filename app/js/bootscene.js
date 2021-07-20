6b2e493d3e1dd6980847f25862278849f249a6e0
unknown <tgr342r@gmail.com>
2021-07-20 15:45:49 +0100

    ball move

--- app/js/bootscene.js
+++ app/js/bootscene.js
@@ -1,39 +1,39 @@
 class BootScene extends Phaser.Scene {
-    kick = false;
-    constructor() {
-        super({ key: 'BootScene' });
-    }
+  kick = false;
+  constructor() {
+    super({ key: "BootScene" });
+  }
 
-    preload() {
-        this.load.image('startButtonImg', '/assets/images/startButton.png');
-        this.load.image('backgroundImg', '/assets/images/pong_background.png');
-        this.load.image('arrowImg', '/assets/images/arrow.png');
-        this.load.image('acceptButtonImg', '/assets/images/acceptButton.png');
-        this.load.image('ballImg', '/assets/images/ball.png');
-        this.load.image('mBackImg', '/assets/images/mobileBackground.png');
-        this.load.image('QrAndroid', '/assets/images/QrAndroid.png');
-    }
+  preload() {
+    this.load.image("startButtonImg", "/assets/images/startButton.png");
+    this.load.image("backgroundImg", "/assets/images/pong_background.png");
+    this.load.image("arrowImg", "/assets/images/arrow.png");
+    this.load.image("acceptButtonImg", "/assets/images/acceptButton.png");
+    this.load.image("ballImg", "/assets/images/ball.png");
+    this.load.image("mBackImg", "/assets/images/mobileBackground.png");
+    this.load.image("QrAndroid", "/assets/images/QrAndroid.png");
+  }
 
-    create() {
-        window.addEventListener('resize', resize);
-        window.addEventListener("orientationchange", resize);
-        resize();
-        // -- inicia uma nova Scene
-        //this.scene.start('MenuScene');
-        socket.on('valida', valida =>{
-            // console.log("adeus");
-            this.scene.start('BlockGame');
-            this.kick = true;
-        });
+  create() {
+    window.addEventListener("resize", resize);
+    window.addEventListener("orientationchange", resize);
+    resize();
+    // -- inicia uma nova Scene
+    //this.scene.start('MenuScene');
+    socket.on("valida", (valida) => {
+      // console.log("adeus");
+      this.scene.start("BlockGame");
+      this.kick = true;
+    });
 
-            if (globalVariable.isDesktop) {
-                if (!this.kick) {
-                this.scene.start('MenuSceneDesktop');
-                // console.log('Desktop');
-                }
-            } else {
-                this.scene.start('MenuSceneMobile');
-                // console.log('Mobile');
-            }
+    if (globalVariable.isDesktop) {
+      if (!this.kick) {
+        this.scene.start("DesktopScene"); // alterado para verificar o jogo no descktop. Voltar a meter ""MenuSceneDesktop"""
+        // console.log('Desktop');
+      }
+    } else {
+      this.scene.start("MenuSceneMobile");
+      // console.log('Mobile');
     }
+  }
 }
--- app/js/desktopscene.js
+++ app/js/desktopscene.js
@@ -1,29 +1,73 @@
 class DesktopScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'DesktopScene' });
-    }
+  constructor() {
+    super({ key: "DesktopScene" });
+  }
 
-    create() {
-        let { width, height } = this.sys.game.canvas;
-        //adiciona uma imagem de fundo ao menu
-        const backgroundImage = this.add.image(
-            this.sys.game.canvas.width / 2,
-            this.sys.game.canvas.height / 2,
-            'backgroundImg'
-        );
+  create() {
+    let XBal = this.sys.game.canvas.width / 2;
+    let YBal = this.sys.game.canvas.height / 2;
+    let VxBal = 0;
+    let VyBal = 0;
+    let VspeedBal = 5;
 
-        // let scaleX = this.cameras.main.width / backgroundImage.width;
-        let scaleY = this.cameras.main.height / backgroundImage.height;
+    var VxBal = Phaser.Math.Between(-5, 5);
+    var VyBal = Phaser.Math.Between(-5, 5);
 
-        backgroundImage.setScale(scaleY).setScrollFactor(0);
-        const ballImage = this.add
-            .image(
-                this.sys.game.canvas.width / 2,
-                this.sys.game.canvas.height / 2,
-                'ballImg'
-            )
-            .setScale(0.02, 0.02);
-    }
+    console.log("VxBal ->" + VxBal);
+    console.log("VyBal ->" + VxBal);
 
-    update() {}
+    let { width, height } = this.sys.game.canvas;
+    //adiciona uma imagem de fundo ao menu
+    const backgroundImage = this.add.image(
+      this.sys.game.canvas.width / 2,
+      this.sys.game.canvas.height / 2,
+      "backgroundImg"
+    );
+
+    // let scaleX = this.cameras.main.width / backgroundImage.width;
+    let scaleY = this.cameras.main.height / backgroundImage.height;
+
+    backgroundImage.setScale(scaleY).setScrollFactor(0);
+    const ballImage = this.add
+      .image(XBal, YBal, "ballImg")
+      .setScale(0.02, 0.02);
+  }
+
+  update() {
+    XBal += 1; //posBalVx * speedBal;
+    YBal += 1; //posBalVy * speedBal;
+  }
+  // x += vx*dt;
+  // y += vy*dt;
+
+  // if ( y + blob_radius > height ) {
+  //   //vx = ????;  // fix this!
+  //   //vy = ????;  // fix this!
+  // }
+
+  // if ( (x - blob_radius < 0) | (x + blob_radius > width) ) {
+  //    //vx = ????;  // fix this!
+  //    //vy = ????;  // fix this!
+  // }
+
+  // if ( (y - blob_radius < ypaddle) & abs(x - xpaddle) < paddle_width/2) {
+  //   //vx = ????;  // fix this!
+  //   //vy = ????;  // fix this!
+  // }
+
+  // // Move the paddle
+  // if (keyIsDown(LEFT_ARROW)) {
+  //    xpaddle += -10;
+  // }
+
+  // if (keyIsDown(RIGHT_ARROW)) {
+  //    xpaddle += 10;
+  // }
+
+  // // Draw axes and other stuff
+  // // This will clear the screen and re-draw it
+  // display();
+
+  // drawBlob(x,y,vx,vy,blob_radius);
+  // drawLine(xpaddle-paddle_width/2,ypaddle,xpaddle+paddle_width/2,ypaddle);
 }
