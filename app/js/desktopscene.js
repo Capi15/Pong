046bfb0096010b8424957404c5c3761d54619a7a
unknown <tgr342r@gmail.com>
2021-07-20 17:07:16 +0100

    ball Verify 8

--- app/js/desktopscene.js
+++ app/js/desktopscene.js
@@ -33,8 +33,8 @@ class DesktopScene extends Phaser.Scene {
   }
 
   update() {
-    this.XBal += VxBal; //posBalVx * speedBal;
-    this.YBal += VyBal; //posBalVy * speedBal;
+    this.XBal += this.VxBal; //posBalVx * speedBal;
+    this.YBal += this.VyBal; //posBalVy * speedBal;
     this.ballImage.setPosition(this.XBal, this.YBal);
   }
   // x += vx*dt;
