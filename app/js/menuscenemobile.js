bf1a0f702128ce1dfce45aad17a221708ee771ae
Capi15 <capicapi1515@gmail.com>
2021-07-26 20:29:37 +0100

    Testing my logs 14

--- app/js/menuscenemobile.js
+++ app/js/menuscenemobile.js
@@ -109,8 +109,12 @@ class MenuSceneMobile extends Phaser.Scene {
             this
         );
 
-        socket.on('MostraComando', (direita) => {
-            this.scene.start('MobileScene', { direita: direita });
+        // socket.on('MostraComando', (direita) => {
+        //     this.scene.start('MobileScene', { direita: direita });
+        // });
+
+        socket.on('MostraComando', () => {
+            this.scene.start('MobileScene');
         });
 
         socket.on('SalaDeEspera', () => {
