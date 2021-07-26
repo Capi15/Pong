6d4a844b29de3a4762fd46ca2bf3b3a480a2d99e
Capi15 <capicapi1515@gmail.com>
2021-07-26 16:46:37 +0100

    o m t5

--- app/js/mobilescene.js
+++ app/js/mobilescene.js
@@ -32,91 +32,6 @@ class MobileScene extends Phaser.Scene {
         }
     }
 
-    // controls() {
-    //     if (window.innerHeight > window.innerWidth) {
-    //         //portrait
-    //         const aThird = this.cameras.main.width / 3;
-    //         this.rightClick = this.add
-    //             .sprite(
-    //                 this.cameras.main.width / 2,
-    //                 this.cameras.main.height / 2 - aThird,
-    //                 'arrowImg',
-    //                 this.rightInput
-    //             )
-    //             .setInteractive()
-    //             .setScale(0.2);
-
-    //         this.rightClick.angle = -90;
-
-    //         this.rightClick.on(
-    //             'gameobjectdown',
-    //             function (pointer) {
-    //                 this.rightClick.setScale(0.3);
-    //             },
-    //             this
-    //         );
-
-    //         this.leftClick = this.add
-    //             .sprite(
-    //                 this.cameras.main.width / 2,
-    //                 this.cameras.main.height / 2 + aThird,
-    //                 'arrowImg',
-    //                 this.leftInput
-    //             )
-    //             .setInteractive()
-    //             .setScale(0.2);
-
-    //         this.leftClick.on(
-    //             'gameobjectdown',
-    //             function (pointer) {
-    //                 this.leftClick.setScale(0.3);
-    //             },
-    //             this
-    //         );
-
-    //         this.leftClick.setFlipX(-1);
-
-    //         this.rightClick.on(
-    //             'gameobjectup',
-    //             function (pointer) {
-    //                 this.rightClick.setScale(0.2);
-    //             },
-    //             this
-    //         );
-    //         this.leftClick.angle = -90;
-
-    //         this.leftClick.on(
-    //             'gameobjectup',
-    //             function (pointer) {
-    //                 this.leftClick.setScale(0.2);
-    //             },
-    //             this
-    //         );
-    //     } else {
-    //         //landscape
-    //         const aSixth = this.cameras.main.width / 6;
-    //         this.rightClick = this.add
-    //             .sprite(
-    //                 this.cameras.main.width / 2 + aSixth,
-    //                 this.cameras.main.height / 2,
-    //                 'arrowImg',
-    //                 this.rightInput
-    //             )
-    //             .setInteractive()
-    //             .setScale(0.2);
-
-    //         this.leftClick = this.add
-    //             .sprite(
-    //                 this.cameras.main.width / 2 - aSixth,
-    //                 this.cameras.main.height / 2,
-    //                 'arrowImg',
-    //                 this.leftInput
-    //             )
-    //             .setInteractive()
-    //             .setScale(0.2);
-    //     }
-    // }
-
     controls() {
         if (window.innerHeight > window.innerWidth) {
             //portrait
@@ -134,7 +49,7 @@ class MobileScene extends Phaser.Scene {
             this.rightClick.angle = -90;
 
             this.rightClick.on(
-                this.input.activePointer.isDown,
+                'pointerdown',
                 function (pointer) {
                     this.rightClick.setScale(0.3);
                 },
@@ -152,7 +67,7 @@ class MobileScene extends Phaser.Scene {
                 .setScale(0.2);
 
             this.leftClick.on(
-                this.input.activePointer.isDown,
+                'pointerdown',
                 function (pointer) {
                     this.leftClick.setScale(0.3);
                 },
@@ -162,7 +77,7 @@ class MobileScene extends Phaser.Scene {
             this.leftClick.setFlipX(-1);
 
             this.rightClick.on(
-                this.input.activePointer.isUp,
+                'pointerup',
                 function (pointer) {
                     this.rightClick.setScale(0.2);
                 },
@@ -171,7 +86,7 @@ class MobileScene extends Phaser.Scene {
             this.leftClick.angle = -90;
 
             this.leftClick.on(
-                this.input.activePointer.isUp,
+                'pointerup',
                 function (pointer) {
                     this.leftClick.setScale(0.2);
                 },
@@ -190,14 +105,6 @@ class MobileScene extends Phaser.Scene {
                 .setInteractive()
                 .setScale(0.2);
 
-            this.rightClick.on(
-                this.input.activePointer.isDown,
-                function (pointer) {
-                    this.rightClick.setScale(0.3);
-                },
-                this
-            );
-
             this.leftClick = this.add
                 .sprite(
                     this.cameras.main.width / 2 - aSixth,
@@ -207,32 +114,125 @@ class MobileScene extends Phaser.Scene {
                 )
                 .setInteractive()
                 .setScale(0.2);
-
-            this.leftClick.on(
-                this.input.activePointer.isDown,
-                function (pointer) {
-                    this.leftClick.setScale(0.3);
-                },
-                this
-            );
-
-            this.leftClick.setFlipX(-1);
-
-            this.rightClick.on(
-                this.input.activePointer.isUp,
-                function (pointer) {
-                    this.rightClick.setScale(0.2);
-                },
-                this
-            );
-
-            this.leftClick.on(
-                this.input.activePointer.isDown,
-                function (pointer) {
-                    this.leftClick.setScale(0.2);
-                },
-                this
-            );
         }
     }
+
+    // controls() {
+    //     if (window.innerHeight > window.innerWidth) {
+    //         //portrait
+    //         const aThird = this.cameras.main.width / 3;
+    //         this.rightClick = this.add
+    //             .sprite(
+    //                 this.cameras.main.width / 2,
+    //                 this.cameras.main.height / 2 - aThird,
+    //                 'arrowImg',
+    //                 this.rightInput
+    //             )
+    //             .setInteractive()
+    //             .setScale(0.2);
+
+    //         this.rightClick.angle = -90;
+
+    //         this.rightClick.on(
+    //             this.input.activePointer.isDown,
+    //             function (pointer) {
+    //                 this.rightClick.setScale(0.3);
+    //             },
+    //             this
+    //         );
+
+    //         this.leftClick = this.add
+    //             .sprite(
+    //                 this.cameras.main.width / 2,
+    //                 this.cameras.main.height / 2 + aThird,
+    //                 'arrowImg',
+    //                 this.leftInput
+    //             )
+    //             .setInteractive()
+    //             .setScale(0.2);
+
+    //         this.leftClick.on(
+    //             this.input.activePointer.isDown,
+    //             function (pointer) {
+    //                 this.leftClick.setScale(0.3);
+    //             },
+    //             this
+    //         );
+
+    //         this.leftClick.setFlipX(-1);
+
+    //         this.rightClick.on(
+    //             this.input.activePointer.isUp,
+    //             function (pointer) {
+    //                 this.rightClick.setScale(0.2);
+    //             },
+    //             this
+    //         );
+    //         this.leftClick.angle = -90;
+
+    //         this.leftClick.on(
+    //             this.input.activePointer.isUp,
+    //             function (pointer) {
+    //                 this.leftClick.setScale(0.2);
+    //             },
+    //             this
+    //         );
+    //     } else {
+    //         //landscape
+    //         const aSixth = this.cameras.main.width / 6;
+    //         this.rightClick = this.add
+    //             .sprite(
+    //                 this.cameras.main.width / 2 + aSixth,
+    //                 this.cameras.main.height / 2,
+    //                 'arrowImg',
+    //                 this.rightInput
+    //             )
+    //             .setInteractive()
+    //             .setScale(0.2);
+
+    //         this.rightClick.on(
+    //             this.input.activePointer.isDown,
+    //             function (pointer) {
+    //                 this.rightClick.setScale(0.3);
+    //             },
+    //             this
+    //         );
+
+    //         this.leftClick = this.add
+    //             .sprite(
+    //                 this.cameras.main.width / 2 - aSixth,
+    //                 this.cameras.main.height / 2,
+    //                 'arrowImg',
+    //                 this.leftInput
+    //             )
+    //             .setInteractive()
+    //             .setScale(0.2);
+
+    //         this.leftClick.on(
+    //             this.input.activePointer.isDown,
+    //             function (pointer) {
+    //                 this.leftClick.setScale(0.3);
+    //             },
+    //             this
+    //         );
+
+    //         this.leftClick.setFlipX(-1);
+
+    //         this.rightClick.on(
+    //             this.input.activePointer.isUp,
+    //             function (pointer) {
+    //                 this.rightClick.setScale(0.2);
+    //             },
+    //             this
+    //         );
+
+    //         this.leftClick.on(
+    //             this.input.activePointer.isDown,
+    //             function (pointer) {
+    //                 this.leftClick.setScale(0.2);
+    //             },
+    //             this
+    //         );
+    //     }
+    // }
 }
